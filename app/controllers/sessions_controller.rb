c003a39a881c846ec611ad9f12a3da7cc4bb6249
81ch <yaich1.8ichi@gmail.com>
2017-08-05 03:40:04 +0000

    user login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,33 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    email.params[:sessions][:email].downcase
+    password.params[:sessions][:password]
+    if login(email, password)
+      flash[:success] = 'ログインに成功しました'
+      redirect_to @user
+      flash[:danger] = 'ログインに失敗しました'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'ログアウトしました'
+    redirect_to root_url
+  end
+  
+  private
+  
+  def login(email, password)
+    @user = User.find_by(email: email)
+    if @user && @user.authenticate(password)
+      session[:user_id] = @user.id
+      return true
+    else
+      return false
+    end
+  end
+end
